9aeac1a68bc8c159105b6961e1f73af1762568b8
ZStop <569643843@qq.com>
2020-07-17 17:56:02 +0800

    feat: js-module-demo

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: ADI
+ * @Date: 2020-07-17 10:26:07
+ * @LastEditors: ADI
+ * @LastEditTime: 2020-07-17 16:40:49
+ */
+import sayName from "./modules/sayname.js";
+import vue from "vue";
+
+sayName("ADI");
+console.log("vue", vue);
---
+++ src/modules/sayname.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: ADI
+ * @Date: 2020-07-17 10:26:07
+ * @LastEditors: ADI
+ * @LastEditTime: 2020-07-17 10:32:42
+ */
+const sayName = (name = "") => {
+  console.log(`my name is : ${name}`);
+};
+
+export default sayName;
